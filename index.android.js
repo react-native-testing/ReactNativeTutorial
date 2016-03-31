671711f5e7a2c50a9ff60febef061cb63a257f30
adrianderose6 <adrian.derose@photochute.io>
2016-03-30 21:17:21 -0700

    Added files via upload

--- index.android.js
+++ index.android.js
@@ -16,7 +16,7 @@ class ReactNativeTutorial extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Hello World!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
